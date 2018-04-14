e5f1b8c32c40ce2447eb5a8218d54e71d0f78d59
Arjun Kalburgi <askalburgi@gmail.com>
2018-04-14 17:45:58 -0600

    Beginning of the list

---
+++ assets/js/listmaker.js
@@ -0,0 +1,353 @@
+var list = [
+    {
+        "name": "Dorinku",
+        "place_id": "ChIJ7entTw4ioFMRJEb9P-xsU2M"
+    },
+    {
+        "name": "Cafe Mosaics",
+        "place_id": "ChIJ6YXslhwioFMRKiBMjNaa_2Y"
+    },
+    {
+        "name": "Hanjan",
+        "place_id": "ChIJ6fqtHSwfoFMR7CEvWZoiuzA"
+    },
+    {
+        "name": "Baijiu Bar",
+        "place_id": "ChIJKcXdPUkioFMRBspqKB4GLqo"
+    },
+    {
+        "name": "Scoop N' Roll",
+        "place_id": "ChIJK-HZ_yUfoFMRVQNRiPtg-yE"
+    },
+    {
+        "name": "Mayday Dogs",
+        "place_id": "ChIJKcXdPUkioFMRqIerQNhliKw"
+    },
+    {
+        "name": "The Holy Roller",
+        "place_id": "ChIJPwFzNA4ioFMRWBv9alBJ1Vw"
+    },
+    {
+        "name": "Have Mercy Southern Table & Bar",
+        "place_id": "ChIJYeWoMw4ioFMR0_Rrlxzlc04"
+    },
+    {
+        "name": "Canteen",
+        "place_id": "ChIJgXmLeNMjoFMR8UurMIGPScM"
+    },
+    {
+        "name": "Artisan Resto-Cafe",
+        "place_id": "ChIJj8y9RBsioFMRUOQXLACd-HE"
+    },
+    {
+        "name": "New York Bagel Cafe",
+        "place_id": "ChIJz-Jn3BEioFMRrmPLB9LnyVs"
+    },
+    {
+        "name": "Under The High Wheel",
+        "place_id": "ChIJC8eyWg4ioFMREEvBaBVDbvY"
+    },
+    {
+        "name": "Urban Diner",
+        "place_id": "ChIJT7wcBysioFMR54-_-DnmDhw"
+    },
+    {
+        "name": "Café Linnea",
+        "place_id": "ChIJmztXI88joFMRKxfHzlHJte0"
+    },
+    {
+        "name": "Cafe Bicyclette",
+        "place_id": "ChIJ1byBhnAioFMR9L65k14J150"
+    },
+    {
+        "name": "District Café & Bakery",
+        "place_id": "ChIJk7ry6jkioFMRcA8fOkgCzeY"
+    },
+    {
+        "name": "SPLASH POKE",
+        "place_id": "ChIJf5EkaTcioFMRg7sAT4-Ikdg"
+    },
+    {
+        "name": "'Ono Poke Co.",
+        "place_id": "ChIJQSNNdkgioFMR0m8X7S3KU6w"
+    },
+    {
+        "name": "Pip",
+        "place_id": "ChIJ3YuZ3g8ioFMRdWTFgAN2QyA"
+    },
+    {
+        "name": "Bar Bricco",
+        "place_id": "ChIJzdLZwEcioFMROKJE1fWtiYw"
+    },
+    {
+        "name": "Uccelino",
+        "place_id": ""
+    },
+    {
+        "name": "Malt & Mortar",
+        "place_id": ""
+    },
+    {
+        "name": "Tres Carnales Taqueria",
+        "place_id": ""
+    },
+    {
+        "name": "Rostizado",
+        "place_id": ""
+    },
+    {
+        "name": "Tzin Wine & Tapas Ltd",
+        "place_id": ""
+    },
+    {
+        "name": "Monument",
+        "place_id": ""
+    },
+    {
+        "name": "XO Bistro + Bar",
+        "place_id": ""
+    },
+    {
+        "name": "Japonais Bistro",
+        "place_id": ""
+    },
+    {
+        "name": "Snowy dessert",
+        "place_id": ""
+    },
+    {
+        "name": "Let eat snow",
+        "place_id": ""
+    },
+    {
+        "name": "Farrow",
+        "place_id": ""
+    },
+    {
+        "name": "Rg rd",
+        "place_id": ""
+    },
+    {
+        "name": "Tiffin",
+        "place_id": ""
+    },
+    {
+        "name": "High voltage food & coffee bar",
+        "place_id": ""
+    },
+    {
+        "name": "The tea girl",
+        "place_id": ""
+    },
+    {
+        "name": "Doughnut party",
+        "place_id": ""
+    },
+    {
+        "name": "Northern chicken",
+        "place_id": ""
+    },
+    {
+        "name": "De Dutch",
+        "place_id": ""
+    },
+    {
+        "name": "Parlour",
+        "place_id": ""
+    },
+    {
+        "name": "Urbano pizza",
+        "place_id": ""
+    },
+    {
+        "name": "Sugarbowl",
+        "place_id": ""
+    },
+    {
+        "name": "High level diner",
+        "place_id": ""
+    },
+    {
+        "name": "Love Pizza",
+        "place_id": ""
+    },
+    {
+        "name": "Blaze pizza",
+        "place_id": ""
+    },
+    {
+        "name": "Block 1912",
+        "place_id": ""
+    },
+    {
+        "name": "It dog",
+        "place_id": ""
+    },
+    {
+        "name": "Rosso",
+        "place_id": ""
+    },
+    {
+        "name": "Izakaya tomo",
+        "place_id": ""
+    },
+    {
+        "name": "Ohana donuterie",
+        "place_id": ""
+    },
+    {
+        "name": "Rebel food & drink",
+        "place_id": ""
+    },
+    {
+        "name": "Dadeo's",
+        "place_id": ""
+    },
+    {
+        "name": "Cibo bistro",
+        "place_id": ""
+    },
+    {
+        "name": "Red ox inn",
+        "place_id": ""
+    },
+    {
+        "name": "Huma",
+        "place_id": ""
+    },
+    {
+        "name": "Solstice",
+        "place_id": ""
+    },
+    {
+        "name": "Leva cafe",
+        "place_id": ""
+    },
+    {
+        "name": "Bistro praha",
+        "place_id": ""
+    },
+    {
+        "name": "Thanh thanh",
+        "place_id": ""
+    },
+    {
+        "name": "Syphay",
+        "place_id": ""
+    },
+    {
+        "name": "Nomchuks",
+        "place_id": ""
+    },
+    {
+        "name": "Parkallen pizza/restaurant",
+        "place_id": ""
+    },
+    {
+        "name": "Packrat louie",
+        "place_id": ""
+    },
+    {
+        "name": "Beer revolution",
+        "place_id": ""
+    },
+    {
+        "name": "Pazzo pazzo",
+        "place_id": ""
+    },
+    {
+        "name": "Jacks pizza cafe",
+        "place_id": ""
+    },
+    {
+        "name": "Stone & wheel",
+        "place_id": ""
+    },
+    {
+        "name": "Little brick",
+        "place_id": ""
+    },
+    {
+        "name": "Blue plate diner",
+        "place_id": ""
+    },
+    {
+        "name": "Gama cafe",
+        "place_id": ""
+    },
+    {
+        "name": "Woodrack cafe",
+        "place_id": ""
+    },
+    {
+        "name": "Art of cake",
+        "place_id": ""
+    },
+    {
+        "name": "Duchess",
+        "place_id": ""
+    },
+    {
+        "name": "Lockstock coffee",
+        "place_id": ""
+    },
+    {
+        "name": "La boule",
+        "place_id": ""
+    },
+    {
+        "name": "Moth cafe",
+        "place_id": ""
+    },
+    {
+        "name": "Crum coffee bar",
+        "place_id": ""
+    },
+    {
+        "name": "Bru coffee + beerhouse ",
+        "place_id": ""
+    },
+    {
+        "name": "Square 1 coffee",
+        "place_id": ""
+    },
+    {
+        "name": "Monument coffee bar",
+        "place_id": ""
+    },
+    {
+        "name": "Credo coffee",
+        "place_id": ""
+    },
+    {
+        "name": "High level diner",
+        "place_id": ""
+    },
+    {
+        "name": "Workshop eatery",
+        "place_id": ""
+    }
+]; 
+
+// http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+// var shuffledlist = shuffle(list);
+function shuffle(sourceArray) {
+    for (var i = 0; i < sourceArray.length - 1; i++) {
+        var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+        var temp = sourceArray[j];
+        sourceArray[j] = sourceArray[i];
+        sourceArray[i] = temp;
+    }
+    return sourceArray;
+}
+
+var featuredPlace = list[0]; 
+console.log(featuredPlace.place_id); 
+
+// $.each(obj, function (key, object) {
+//     var name = object.name; 
+//     var place_id = object.place_id; 
+
+
+// });
